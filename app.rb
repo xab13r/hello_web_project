3b90a67f9a883636edb4a4574facdceb0bd75c3f
lorenzo <2306227+xab13r@users.noreply.github.com>
2022-10-20 11:25:41 +0100

    Set up completed. First challenge completed

---
+++ app.rb
@@ -0,0 +1,24 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Application < Sinatra::Base
+  # This allows the app code to refresh
+  # without having to restart the server.
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/hello' do
+    name = params[:name]
+    
+    return "Hello #{name}"
+  end
+  
+  post '/submit' do
+    name = params[:name]
+    message = params[:message]
+    
+    return "Thanks #{name}, you sent this message: \"#{message}\""
+  end
+end
+
